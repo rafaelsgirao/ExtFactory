2a8dfe619e3b0a67548ed7215c1564fe77d63ede
Rafael Girão <git@rafael.ovh>
2023-12-04 11:46:00 +0000

    localCDN: add some meta-info

--- packages/localCDN/default.nix
+++ packages/localCDN/default.nix
@@ -9,6 +9,13 @@ rec {
     rev = "v${version}";
     sha256 = "sha256-icJeMIqNcHXH2UQWJ5BGX/rtyllUSzf2QCny7c6+GQg=";
   };
+
+  meta = with lib; {
+    description = "An extension that emulates Content Delivery Networks to improve your online privacy.";
+    license = licenses.mpl20;
+    # maintainers = 
+    homepage = "TODO";
+  };
 }
 ''
     mkdir $out
