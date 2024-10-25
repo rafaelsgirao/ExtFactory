596fc9d6e9191a0816bf271d24fa74e82a64059d
Rafael Girão <git@rafael.ovh>
2024-10-26 00:24:21 +0100

    chore: update nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "flake:nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -60,10 +60,8 @@
           check.enable = true;
           settings.hooks = {
             actionlint.enable = true;
-            deadnix = {
-                enable = true;
-                settings.edit = true;
-            };
+            deadnix.enable = true;
+            deadnix.settings.edit = true;
             statix.enable = true;
             markdownlint.enable = true;
             prettier.enable = true;
