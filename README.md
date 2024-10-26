a91d309f317187032ac985b7cd928941e26c0861
Rafael Girão <git@rafael.ovh>
2024-10-26 01:28:34 +0100

    feat(localcdn): update

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - [ ] add minimal frontend
 - [ ] add auto package updates (see [chromium-web-store](https://github.com/NeverDecaf/chromium-web-store?tab=readme-ov-file#if-you-are-a-chrome-extension-developer))
 - [ ] auto update package PR when upstream creates new tag, a-la nixpkgs
+- [ ] auto sign extensions
--- packages/localcdn/default.nix
+++ packages/localcdn/default.nix
@@ -8,11 +8,11 @@
 runCommand "localCDN"
   rec {
     pname = "LocalCDN";
-    version = "2.6.59";
+    version = "2.6.74";
     src = fetchgit {
       url = "https://codeberg.org/nobody/${pname}";
       rev = "v${version}";
-      sha256 = "sha256-icJeMIqNcHXH2UQWJ5BGX/rtyllUSzf2QCny7c6+GQg=";
+      sha256 = "sha256-Nk1sHv0uZetErFO3lZF4MGNi3wQuy2CCoSAXXyXv7mY=";
     };
 
     meta = with lib; {
