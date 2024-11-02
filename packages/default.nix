4f8353a89e19d3f24aa5d6b505bf16b9184bcd6b
Rafael Girão <git@rafael.ovh>
2024-11-02 19:15:53 +0000

    feat: package crx util

--- packages/default.nix
+++ packages/default.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 rec {
+
+  # Meta packages.
+  crx = (pkgs.callPackage ./crx { nodejs = pkgs.nodejs_18; }).package;
+
+  # Web extensions.
   darkreader = pkgs.callPackage ./darkreader { };
   localcdn = pkgs.callPackage ./localcdn { };
   ublockorigin = pkgs.callPackage ./ublockorigin { };
